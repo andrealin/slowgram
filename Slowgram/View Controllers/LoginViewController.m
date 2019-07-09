33095f357f28e54759f73909f6afdc6a9fc4f486
Andrea Lin <drealin@fb.com>
2019-07-08 17:22:52 -0700

    user registration

--- Slowgram/View Controllers/LoginViewController.m
+++ Slowgram/View Controllers/LoginViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "LoginViewController.h"
+#import "Parse/Parse.h"
 
 @interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
 
 @end
 
@@ -18,5 +21,27 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
-
+- (IBAction)signUpClicked:(id)sender {
+    [self registerUser];
+}
+- (void)registerUser {
+    // initialize a user object
+    PFUser *newUser = [PFUser user];
+    
+    // set user properties
+    newUser.username = self.usernameField.text;
+//    newUser.email = self.emailField.text;
+    newUser.password = self.passwordField.text;
+    
+    // call sign up function on the object
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User registered successfully");
+            
+            // manually segue to logged in view
+        }
+    }];
+}
 @end
